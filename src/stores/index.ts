dec8fc956484e8369eec6225a5501e15be7719b0
minhck <minhck@bap.jp>
2024-07-08 16:49:21 +0700

    Config react router

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,13 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
+
+import { createReduxHistoryContext } from 'redux-first-history';
+import { createBrowserHistory } from 'history';
+
 import rootReducer from './root-reducers';
 import rootSaga from './root-saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
+const { createReduxHistory, routerMiddleware, routerReducer } = createReduxHistoryContext({
+  history: createBrowserHistory(),
+});
+
 const store = configureStore({
-  reducer: rootReducer,
-  middleware: getDefaultMiddleware => getDefaultMiddleware({ thunk: false }).concat(sagaMiddleware),
+  reducer: {
+    ...rootReducer,
+    router: routerReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({ thunk: false }).concat(sagaMiddleware).concat(routerMiddleware),
 });
 
 sagaMiddleware.run(rootSaga);
@@ -15,4 +27,5 @@ sagaMiddleware.run(rootSaga);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
+export const history = createReduxHistory(store);
 export default store;
